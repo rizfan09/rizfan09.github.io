60423b9e4e73032b0cad1cf66c9d8ee9d6e8b248
rizfan09 <92281587+rizfan09@users.noreply.github.com>
2022-12-04 20:11:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const menutoggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+menutoggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
